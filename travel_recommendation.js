0074491667298acffeecf37d17cd57bcfa01c83d
pranay koppula <pranay.koppula@xcubelabs.com>
2025-02-04 14:37:09 +0530

    Debugging fetch statement

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,4 +1,4 @@
-const data = fetch("/travel_recommendation_api.json",{"mode":"cors"})
+const data = fetch("travel_recommendation_api.json")
 .then((response) => {
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
